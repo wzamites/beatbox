273799cbfcaf06daeb93d9ceafce26b9970ca421
wzamites <bj_zamites@comcast.net>
2020-02-22 02:19:36 -0500

    add products to CardSection manually

--- src/CardSection.js
+++ src/CardSection.js
@@ -5,86 +5,88 @@ import Button from 'react-bootstrap/Button'
 
 import Image1 from './images/productimages/scm-corpsmaster-snare-colin-mcnutt.png'
 import Image2 from './images/productimages/smap-corpsmaster-signature-snare-john-mapes.png'
-import Image3 from './images/productimages/scm-corpsmaster-snare-colin-mcnutt.png'
-import Image4 from './images/productimages/scm-corpsmaster-snare-colin-mcnutt.png'
-import Image5 from './images/productimages/scm-corpsmaster-snare-colin-mcnutt.png'
-
-
+import Image3 from './images/productimages/srhn-corpsmaster-signature-snare-ralph-hardimon-nylon-tip.png'
+import Image4 from './images/productimages/sth-corpsmaster-signature-snare-thom-hannum.png'
+import Image5 from './images/productimages/sth3-corpsmaster-signature-snare-thom-hannum-piccolo-tip.png'
 
 function CardSection() {
   return (
     <div className="containerLayout">
 
   <Card className="cardContainer" style={{ width: '18rem' }}>
-  <Card.Img variant="top" src={Image1} />
+  <Card.Img variant="top" src={Image1} vspace="75" />
   <Card.Body>
     <Card.Title>Colin Mcnutt</Card.Title>
     <Card.Text>
       Some quick example text to build on the card title and make up the bulk of
       the card's content.
     </Card.Text>
-    <Button variant="light">Go somewhere</Button>
+    <Button variant="dark">Subscribe</Button><span>  </span>
+    <Button variant="light">Add to Cart</Button>
   </Card.Body>
 </Card>
 
 <Card className="cardContainer" style={{ width: '18rem' }}>
-  <Card.Img variant="top" src={Image2} />
+  <Card.Img variant="top" src={Image2} vspace="75"/>
   <Card.Body>
     <Card.Title>John Mapes</Card.Title>
     <Card.Text>
       Some quick example text to build on the card title and make up the bulk of
       the card's content.
     </Card.Text>
-    <Button variant="light">Go somewhere</Button>
+    <Button variant="dark">Subscribe</Button><span>  </span>
+    <Button variant="light">Add to Cart</Button>
   </Card.Body>
 </Card>
 
 <Card className="cardContainer" style={{ width: '18rem' }}>
-  <Card.Img variant="top" src={Image3} />
+  <Card.Img variant="top" src={Image3} vspace="75"/>
   <Card.Body>
     <Card.Title>Ralph Hardimon</Card.Title>
     <Card.Text>
       Some quick example text to build on the card title and make up the bulk of
       the card's content.
     </Card.Text>
-    <Button variant="light">Go somewhere</Button>
+    <Button variant="dark">Subscribe</Button><span>  </span>
+    <Button variant="light">Add to Cart</Button>
   </Card.Body>
 </Card>
 
 <Card className="cardContainer" style={{ width: '18rem' }}>
-<Card.Img variant="top" src={Image1} />
+<Card.Img variant="top" src={Image5} vspace="75"/>
+<Card.Body>
+  <Card.Title>Thom Hannum Piccolo</Card.Title>
+  <Card.Text>
+    Some quick example text to build on the card title and make up the bulk of
+    the card's content.
+  </Card.Text>
+  <Button variant="dark">Subscribe</Button><span>  </span>
+  <Button variant="light">Add to Cart</Button></Card.Body>
+</Card>
+
+<Card className="cardContainer" style={{ width: '18rem' }}>
+<Card.Img variant="top" src={Image1} vspace="75" />
 <Card.Body>
   <Card.Title>Colin Mcnutt</Card.Title>
   <Card.Text>
     Some quick example text to build on the card title and make up the bulk of
     the card's content.
   </Card.Text>
-  <Button variant="light">Go somewhere</Button>
+  <Button variant="dark">Subscribe</Button><span>  </span>
+  <Button variant="light">Add to Cart</Button>
 </Card.Body>
 </Card>
 
 <Card className="cardContainer" style={{ width: '18rem' }}>
-<Card.Img variant="top" src={Image2} />
+<Card.Img variant="top" src={Image4} vspace="75"/>
 <Card.Body>
-  <Card.Title>John Mapes</Card.Title>
+  <Card.Title>Thom Hannum</Card.Title>
   <Card.Text>
     Some quick example text to build on the card title and make up the bulk of
     the card's content.
   </Card.Text>
-  <Button variant="light">Go somewhere</Button>
-</Card.Body>
-</Card>
-
-<Card className="cardContainer" style={{ width: '18rem' }}>
-<Card.Img variant="top" src={Image3} />
-<Card.Body>
-  <Card.Title>Ralph Hardimon</Card.Title>
-  <Card.Text>
-    Some quick example text to build on the card title and make up the bulk of
-    the card's content.
-  </Card.Text>
-  <Button variant="light">Go somewhere</Button>
-</Card.Body>
+  <Button variant="dark">Subscribe</Button><span>  </span>
+  <Button variant="light">Add to Cart</Button></Card.Body>
 </Card>
 
 </div>
