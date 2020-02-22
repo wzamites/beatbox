88e1b11c081f99c11555230015af3c6e56f873df
wzamites <bj_zamites@comcast.net>
2020-02-22 01:34:18 -0500

    add fakeapi

---
+++ src/API/products.js
@@ -0,0 +1,17 @@
+const products = {
+  "count": 1,
+  "next": "?page=1",
+  "prev": null,
+  "results": [
+    {
+      id: 70990774,
+      name: "Spud of the Month Club",
+      description: "The best potatoes in the world. To your door. Every month.",
+      slug: "spud-of-the-month",
+      visible: true,
+      deleted: false,
+      url: "/v1/store/api/products/70990774/"    }
+  ]
+}
+
+export default products;
