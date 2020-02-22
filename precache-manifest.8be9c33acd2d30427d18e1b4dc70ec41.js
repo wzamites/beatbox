9bd09465695168b70334bcd434dd801f1e02ea7f
wzamites <bj_zamites@comcast.net>
2020-02-22 02:33:34 -0500

    Updates

---
+++ precache-manifest.8be9c33acd2d30427d18e1b4dc70ec41.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64dace3e1eaa6407e439a33ec4a2d836",
+    "url": "/beatbox/index.html"
+  },
+  {
+    "revision": "4607edd7481cda0fac4d",
+    "url": "/beatbox/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "4e6450059c7767716b46",
+    "url": "/beatbox/static/css/main.f353f54c.chunk.css"
+  },
+  {
+    "revision": "4607edd7481cda0fac4d",
+    "url": "/beatbox/static/js/2.bc653901.chunk.js"
+  },
+  {
+    "revision": "a52d65c95a2d7ffcb9ff212c4d38dff4",
+    "url": "/beatbox/static/js/2.bc653901.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e6450059c7767716b46",
+    "url": "/beatbox/static/js/main.de353376.chunk.js"
+  },
+  {
+    "revision": "b509493271a1cae510fb",
+    "url": "/beatbox/static/js/runtime-main.9109a98f.js"
+  },
+  {
+    "revision": "b1efdf3ee2b4aecfb5f8a522fac884de",
+    "url": "/beatbox/static/media/beatboxlogo.b1efdf3e.png"
+  },
+  {
+    "revision": "73e39f16c6c6eedb7e967896e69708ea",
+    "url": "/beatbox/static/media/matthijs-smit-bRG2C0FAQEY-unsplash.73e39f16.jpg"
+  },
+  {
+    "revision": "e911a47e03e73251df750a6779f63558",
+    "url": "/beatbox/static/media/scm-corpsmaster-snare-colin-mcnutt.e911a47e.png"
+  },
+  {
+    "revision": "79ac68a0255a5d21ebe3061280122c26",
+    "url": "/beatbox/static/media/smap-corpsmaster-signature-snare-john-mapes.79ac68a0.png"
+  },
+  {
+    "revision": "a623b9fe3ff47318a88b5ec6945ff9ac",
+    "url": "/beatbox/static/media/srhn-corpsmaster-signature-snare-ralph-hardimon-nylon-tip.a623b9fe.png"
+  },
+  {
+    "revision": "ea7f482abe364c6d48a3fce5cab5cdaa",
+    "url": "/beatbox/static/media/sth-corpsmaster-signature-snare-thom-hannum.ea7f482a.png"
+  },
+  {
+    "revision": "2a57c7156b884b1e41814253c3ee939f",
+    "url": "/beatbox/static/media/sth3-corpsmaster-signature-snare-thom-hannum-piccolo-tip.2a57c715.png"
+  }
+]);
