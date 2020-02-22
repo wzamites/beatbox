9ce06f8d6fb80a6e1fc38831987b35ef160d0088
wzamites <bj_zamites@comcast.net>
2020-02-22 02:19:12 -0500

    remove commented api contents

--- src/App.js
+++ src/App.js
@@ -8,23 +8,6 @@ import MyCarousel from './MyCarousel'
 import MyFooter from './MyFooter'
 import CardSection from './CardSection'
 
-
-// const products = {
-//   "count": 1,
-//   "next": "?page=1",
-//   "prev": null,
-//   "results": [
-//     {
-//       id: 70990774,
-//       name: "Spud of the Month Club",
-//       description: "The best potatoes in the world. To your door. Every month.",
-//       slug: "spud-of-the-month",
-//       visible: true,
-//       deleted: false,
-//       url: "/v1/store/api/products/70990774/"    }
-//   ]
-// }
-
 function App() {
   return (
     <div>
