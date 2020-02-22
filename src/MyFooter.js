989d8701d77f76721bbe9d9536eac7d2f24214ab
wzamites <bj_zamites@comcast.net>
2020-02-22 02:41:08 -0500

    change footer

--- src/MyFooter.js
+++ src/MyFooter.js
@@ -3,7 +3,7 @@ import React from 'react';
 function MyFooter() {
   return (
     <footer className="text-center py-3 greyBackground">
-      © {new Date().getFullYear()} <a href="#" >Hello</a>
+      © {new Date().getFullYear()} <a href="#" >beatbox co</a>
     </footer>
   );
 }
