1deed975c7d59cd967a565aa389b805b8ff76495
wzamites <bj_zamites@comcast.net>
2020-02-23 02:01:46 -0500

    update navbar to add sign-ins

--- src/MyNavbar.js
+++ src/MyNavbar.js
@@ -18,24 +18,17 @@ function MyNavbar() {
       <Navbar.Collapse id="basic-navbar-nav">
 
         <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-          </NavDropdown>
-          <Form inline>
-            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-            <Button variant="outline-secondary">Search</Button>
-          </Form>
+          <Nav.Link href="#home">Get Started</Nav.Link>
+          <Nav.Link href="#link">Products</Nav.Link>
+          <Nav.Link href="#how">How It Works</Nav.Link>
         </Nav>
-
-
-        <i class="fas fa-shopping-cart">Hello</i>
-
+        <Nav>
+          <Button variant="outline-secondary">Sign Up</Button>
+          <Nav.Link href="#login">Sign In</Nav.Link>
+          <Nav.Link href="#login">
+            <i class="fas fa-shopping-cart"></i>
+          </Nav.Link>
+        </Nav>
 
         </Navbar.Collapse>
 
