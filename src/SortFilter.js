99a2bfdf50149bf7abf714d8c51a1f9638287a90
wzamites <bj_zamites@comcast.net>
2020-02-28 00:30:27 -0500

    add passable buttons

--- src/SortFilter.js
+++ src/SortFilter.js
@@ -6,15 +6,15 @@ import Button from 'react-bootstrap/Button'
 function SortFilter() {
   return (
   <ButtonToolbar>
-    <Button variant="primary">Primary</Button>
-    <Button variant="secondary">Secondary</Button>
-    <Button variant="success">Success</Button>
-    <Button variant="warning">Warning</Button>
-    <Button variant="danger">Danger</Button>
-    <Button variant="info">Info</Button>
-    <Button variant="light">Light</Button>
-    <Button variant="dark">Dark</Button>
-    <Button variant="link">Link</Button>
+    <Button variant="outline-secondary">Taper</Button>
+    <Button variant="outline-secondary">Material</Button>
+    <Button variant="outline-secondary">Tip Shape</Button>
+    <Button variant="outline-secondary">Tip Material</Button>
+    <Button variant="outline-secondary">Finish</Button>
+    <Button variant="outline-secondary">Diameter</Button>
+    <Button variant="outline-secondary">Series</Button>
+    <Button variant="outline-secondary">Length</Button>
+    <Button variant="outline-secondary">Link</Button>
   </ButtonToolbar>
   )
 }
